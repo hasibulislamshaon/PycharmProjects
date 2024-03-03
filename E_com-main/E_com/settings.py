60126b7e808a64d393cb41244e9ccace93a0bbce
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2024-03-03 14:02:30 +0600

    Update settings.py

--- E_com-main/E_com/settings.py
+++ E_com-main/E_com/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'products',
     'import_export',
 
+
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'E_com.urls'
